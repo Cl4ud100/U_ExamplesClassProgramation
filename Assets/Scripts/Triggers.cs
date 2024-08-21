4879abc892bb6dd0d3fc265b14929c0cee3e30a1
Cl4ud100 <claudio.gonzalezg@uniacc.edu>
2024-08-21 10:08:02 -0400

    Fix Triggers

--- Assets/Scripts/Triggers.cs
+++ Assets/Scripts/Triggers.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Triggers : MonoBehaviour
 {
     public float velocity = 4.5f;
-    public bool _clueFind1 = false;
+    public bool clueFind1 = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        throw new NotImplementedException();
+        if (other.CompareTag("Clue1"))
+        {
+            Debug.Log("Choque con el trigger" + clueFind1);
+        }
     }
 }
