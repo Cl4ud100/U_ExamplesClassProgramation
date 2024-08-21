44f7c707e1abaa39b846081a1c53a712ca9b8a22
Cl4ud100 <claudio.gonzalezg@uniacc.edu>
2024-08-21 09:45:50 -0400

    Update Triggers.cs

--- Assets/Scripts/Triggers.cs
+++ Assets/Scripts/Triggers.cs
@@ -1,7 +1,14 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Triggers : MonoBehaviour
 {
     public float velocity = 4.5f;
     public bool _clueFind1 = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        throw new NotImplementedException();
+    }
 }
